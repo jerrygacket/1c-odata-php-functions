c4b644a1a96bfaaf8de0734acb0311f3716d7e0d
Artem <kryuchkov_artem@mail.ru>
2018-07-06 12:09:44 +0300

    Add files via upload
    Изменена функция загрузки данных из монго. Теперь можно при выгрузке сформировать массив по нужному ключу $key с нужным значением $value.

--- php/50-functions.php
+++ php/50-functions.php
@@ -1,5 +1,4 @@
 <?php
-// запрос данных из 1с по интерфейсу одата в формате json
 function get1cData($client,$userName, $userAccessKey,$fromtable,$select,$where) {
 	$jsonformat = '$format=json;odata=nometadata';
 	$requesturl = $fromtable.'?';
@@ -9,100 +8,37 @@ function get1cData($client,$userName, $userAccessKey,$fromtable,$select,$where) 
 	if (!empty($where)) {
 		$requesturl .= '$filter='.$where.'&';
 	}
-	// expand у меня не заработал.
-	//if (!empty($expand)) {
-	//	$requesturl .= $expand.'&';
-	//}
+	if (!empty($expand)) {
+		$requesturl .= $expand.'&';
+	}
 	$requesturl .= $jsonformat;
+	//print_r($requesturl);
 	$response = $client->request('GET', $requesturl, [
 		'auth' => [$userName, $userAccessKey]
 	]);
+	//$body = $response->getBody()->getContents();
 	$body = $response->getBody();
 	$jsonResponse=json_decode($body,true);
 	return $jsonResponse['value'];
 }
 
-// функции для работы с базой монго
-// загрузить документ в монго базу
-function loadToMongo($mongodb,$dbname,$collectionname, $data) {
-	$collection = $mongodb->$dbname->$collectionname; //берем коллекцию $collectionname
-	foreach ($data as $doc) {
-		$result = $collection->find(['Ref_Key' => $doc['Ref_Key']])->toArray();
-		if (!empty($result)) {
-				$collection->updateOne( ['Ref_Key' => $doc['Ref_Key']], [ '$set' => $doc ] );
-		} else {
-			$insertOneResult = $collection->insertOne( $doc ); //если не находим, то добавляем один
-		}
-	}
-}
-// получить всю коллекцию в массиве вида ('ref_key_из_1с' => 'description')
-// используется для получения списков менеджеров, контрагентов, подразделений
-function getMongoData($mongodb,$dbname,$collectionname) {
+function getMongoData($mongodb,$dbname,$collectionname,$key,$value) {
     $result = array();
-    $collection = $mongodb->$dbname->$collectionname; //берем коллекцию $collectionname
+    $collection = $mongodb->$dbname->$collectionname;
     $search = $collection->find([])->toArray();
     foreach ($search as $tmp) {
-        $result[$tmp['Ref_Key']] = $tmp['Description'];
+        $result[$tmp[$key]] = $tmp[$value] ?? 'не указан';
     }
     return $result;
 }
 
-//поиск контактных лиц по телефону в базе монго
-function searchInMongo($mongodb,$dbname, $phone) {
-	$outstr = array();
-	$collection = $mongodb->$dbname->Catalog_КонтактныеЛицаПартнеров;
-	$result = $collection->find(['КонтактнаяИнформация' => ['$elemMatch' => ['НомерТелефона' => $phone] ]])->toArray();
-	if (empty($result)) {
-		$collection = $mongodb->$dbname->Catalog_Контрагенты;
-		$contragents = $collection->find(['КонтактнаяИнформация' => ['$elemMatch' => ['НомерТелефона' => $phone] ]])->toArray();
-		if (empty($contragents)) {
-			return 'Ничего не найдено';
-		}
-		$collection = $mongodb->$dbname->Catalog_КонтактныеЛицаПартнеров;
-		foreach ($contragents as $contragent) {
-			$outstr['Контрагент'] = $contragent['Description'];
-			$contacts = $collection->find(['Owner_Key' => $contragent['Ref_Key']])->toArray();
-			foreach ($contacts as $contact) {
-				$outstr['Контактное лицо'][] = $contact['Description'];
-			}
-		}
-	} else {
-		$collection = $mongodb->$dbname->Catalog_Контрагенты;
-		foreach ($result as $contact) {
-			$outstr['Контактное лицо'] = $contact['Description'];
-			$contragents = $collection->find(['Ref_Key' => $contact['Owner_Key']])->toArray();
-			foreach ($contragents as $contragent) {
-				$outstr['Контрагент'][] = $contragent['Description'];
-			}
-		}
-		
-	}
-	$outstr['Телефон'] = $phone;
-	return $outstr;
-}
-
-// для канборда
-// получить id колонки по имени. В апи канборда такой функции нет
-function getColumnId ($columns,$colimntitle) {
-    $result = -1;
-    foreach ($columns as $column) {
-        if ( $colimntitle == $column['title'] ) {
-            $result = $column['id'];
-        }
-    }
+function getAllMongoData($mongodb,$dbname,$collectionname) {
+    $result = array();
+    $collection = $mongodb->$dbname->$collectionname;
+    $result = $collection->find([])->toArray();
     return $result;
 }
 
-// остальные функции
-// замена "пробел" на "\пробел". нужно при создании папок на сервере (debian).
-function SpiceEsc($string_to_clear) {
-	$string_to_clear = trim(str_replace(array(" "), "\ ", $string_to_clear)); //удаляем кавычки
-    return $string_to_clear;
-}
-
-// очищаем строку от запрещенных и нежелательных символов.
-// в самбе есть глюк при отображении папок вида "Иванов И. И." при монтировании в Thunar видно что-то типа: _67BF8~D
-// нужно при создании папок на сервере (debian).
 function ClearString($string_to_clear) {
 	$string_to_clear = trim(str_replace(array("\r\n", "\r", "\n", "\t"), " ", $string_to_clear)); //удаляем переносы строки, табуляцию, 
 	$string_to_clear = trim(str_replace(array("\""), "", $string_to_clear)); //удаляем кавычки
@@ -112,41 +48,8 @@ function ClearString($string_to_clear) {
     return $string_to_clear;
 }
 
-// просто заменяем пробелы и минусы на нижнее подчеркивание.
-// чтобы папки создавались однообразно
 function NoSpice($string_to_clear) {
-	$string_to_clear = trim(str_replace(array(" ","."), "_", $string_to_clear)); //удаляем пробелы
-	$string_to_clear = trim(str_replace(array("-"), "_", $string_to_clear)); //удаляем минусы
+	$string_to_clear = trim(str_replace(array(" ","."), "_", $string_to_clear)); //удаляем кавычки
+    $string_to_clear = trim(str_replace(array("-"), "_", $string_to_clear)); //удаляем кавычки
     return $string_to_clear;
-}
-
-// приводим номер телефона к виду 89205634455
-// нужно для определения номера по контрагентам из 1с (asterisk)
-function normalizator($phone) {
-	$number1c = preg_replace("#[^0-9]#","",$phone); //Все не цифры - нах
-	switch ( 1 ) {
-	//1 XX-XX-XX 6
-		case ( preg_match("/^([0-9]{6})$/",$number1c) ):
-			$number1c = '84932'.$number1c;
-			break;
-	//2 7 (XXX) XXX-XX-XX 11
-		case ( preg_match("/^[7]{1}([0-9]{10})$/",$number1c) ):
-			$number1c = '8'.substr($number1c, 1);
-			break;
-	//3 +7 (XXX) XXX-XX-XX 12
-		case ( preg_match("/^[+]{1}[7]{1}([0-9]{10})$/",$number1c) ):
-			$number1c = '8'.substr($number1c, 2);
-			break;
-	//4 8 (XXX) XXX-XX-XX 11
-		case ( preg_match("/^[8]{1}([0-9]{10})$/",$number1c) ):
-			$number1c = $number1c;
-			break;
-	//5 (XXX) XXX-XX-XX 10
-		case ( preg_match("/^([0-9]{10})$/",$number1c) ):
-			$number1c = '8'.$number1c;
-			break;
-		default:
-			$number1c = $number1c;
-	}
-	return $number1c;
 }
---
+++ php/98-class.php
@@ -0,0 +1,92 @@
+<?php
+
+class order
+{
+    private $DocTypes = array('Задолженность','Платежка','Реализация','Чек');
+    private $Даты = array();
+    private $Суммы = array();
+    
+    public $Number = '';
+    public $СуммаДокумента = 0;
+    public $СуммаОплаты = 0;
+    public $СуммаРеализации = 0;
+    public $ДатаЗаказа = '';
+    public $МенеджерЗаказа = '';
+    public $МенеджерКлиента = '';
+    public $Контрагент = '';
+    public $Партнер = '';
+    public $Организация = '';
+    public $КаналПервичногоИнтереса = 'не указан';
+    public $ПоследняяДата = 0;
+    
+    // заполняем даты и суммы отгрузог, платежек, чеков и взаимозачетов
+    function newDoc($DocType, $Date, $Money) {
+        if (!in_array($DocType,$this->DocTypes)) {
+            return false;
+        }
+        if (!array_key_exists($DocType,$this->Даты)) {
+            $this->Даты[$DocType] = array();
+        }
+        $this->Даты[$DocType][] = strtotime(trim($Date));
+        
+        if (!array_key_exists($DocType,$this->Суммы)) {
+            $this->Суммы[$DocType] = array();
+        }
+        $this->Суммы[$DocType][] = trim($Money);
+        return true;
+    }
+    
+    function Собрать($DocType) {
+        if (!array_key_exists($DocType,$this->Даты)) {
+            return '';
+        }
+        $result = '<ol>';
+        foreach ($this->Даты[$DocType] as $key => $Дата) {
+            $result .= '<li>'.date('d.m.Y',$Дата).'&nbsp;';
+            $result .= $this->Суммы[$DocType][$key].'</li>';
+        }
+        $result .= '</ol>';
+        return $result;
+    }
+    
+    function ОплаченПолностью() {
+        $result = false;
+        $this->СуммаОплаты = array_sum( $this->Суммы['Платежка'] ?? array(0) );
+        $this->СуммаОплаты += array_sum( $this->Суммы['Чек'] ?? array(0) );
+        $this->СуммаОплаты += array_sum( $this->Суммы['Задолженность'] ?? array(0) );
+        // далее нужен ceil, потому что иногда сумма оплаты почемуто
+        // на 3.43232342343653676Е-12 меньше чем сумма документа
+        $result = (ceil($this->СуммаОплаты*100)/100) >= (ceil($this->СуммаДокумента*100)/100);
+        //~ if ($this->Number == '0000-005901'){file_put_contents('debug.log',$result);}
+        return $result;
+    }
+    
+    function РеализованПолностью() {
+        $result = false;
+        $this->СуммаРеализации = array_sum( $this->Суммы['Реализация'] ?? array(0) );
+        // далее нужен ceil, потому что иногда сумма почемуто
+        // на 3.43232342343653676Е-12 меньше чем сумма документа
+        $result = (ceil($this->СуммаРеализации*100)/100) >= (ceil($this->СуммаДокумента*100)/100);
+        //~ if ($this->Number == '0000-005901'){file_put_contents('debug.log',$result);}
+        return $result;
+    }
+    
+    function ЗакрытВЭтотПериод($begin, $end) {
+        $result = false;
+        $ПоследняяДатаЗадолженность = max( $this->Даты['Задолженность'] ?? array(0) );
+        $ПоследняяДатаПлатежка = max( $this->Даты['Платежка'] ?? array(0) );
+        $ПоследняяДатаРеализация = max( $this->Даты['Реализация'] ?? array(0) );
+        $ПоследняяДатаЧек = max( $this->Даты['Чек'] ?? array(0) );
+        $this->ПоследняяДата = max($ПоследняяДатаЗадолженность,$ПоследняяДатаПлатежка,$ПоследняяДатаРеализация,$ПоследняяДатаЧек);
+        if (($ПоследняяДатаПлатежка != 0) or ($ПоследняяДатаЧек != 0)) {
+            if (($this->ПоследняяДата >= $begin) and ($this->ПоследняяДата < $end)) {
+                $result = true;
+            }
+        }
+        return $result;
+    }
+    
+    function СуммаДляУчета() {
+        return $this->СуммаДокумента - array_sum( $this->Суммы['Задолженность'] ?? array(0) );
+    }
+}
